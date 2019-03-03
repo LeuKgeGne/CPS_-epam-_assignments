9f6610c9f20cb356e5ab6624a11a4f6503d5c337
Eugene <eugene.blizniuk@gmail.com>
2019-03-04 00:29:08 +0300

    Corrected The Code Convention

---
+++ bsuir/CPS/TheFirstProject/Manipulations.java
@@ -0,0 +1,23 @@
+package bsuir.CPS.TheFirstProject;
+
+public class Manipulations {
+    public static boolean isIsoscelesTriangle(int a, int b, int c) {
+        return (a == b || b == c || a == c) ? true : false;
+    }
+
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
+        return (a == b && b == c) ? true : false;
+    }
+
+    public static boolean isRightTriangle(int a, int b, int c) {
+        if ((c * c) == ((a * a) + (b * b))) {
+            return true;
+        } else if ((b * b) == ((a * a) + (c * c))) {
+            return true;
+        } else if ((a * a) == ((b * b) + (c * c))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
